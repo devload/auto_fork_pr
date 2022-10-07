b539e8cd0bf2ea980872bbdbaf3b1d1e52d51182
roh.sunghyun <devload@onycom.com>
2022-10-07 23:53:49 +0900

    Project init

--- index.js
+++ index.js
@@ -1,21 +1,70 @@
 const core = require('@actions/core');
+const github = require('@actions/github');
 const wait = require('./wait');
 
 
 // most @actions toolkit packages have async methods
 async function run() {
   try {
-    const ms = core.getInput('milliseconds');
-    core.info(`Waiting ${ms} milliseconds ...`);
 
-    core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    await wait(parseInt(ms));
-    core.info((new Date()).toTimeString());
+      const githubToken = core.getInput('githubToken');
+      const targetOwner = core.getInput('targetOwner');
+      const targetRepository = core.getInput('targetRepository');
+      const targetBranch = core.getInput('targetBranch');
 
-    core.setOutput('time', new Date().toTimeString());
+      const prOwner = core.getInput('prOwner');
+      const prRepository = core.getInput('prRepository');
+      const prBranch = core.getInput('prBranch');
+      const prBaseBranch = core.getInput('prBaseBranch');
+
+    const {sha:mainBranchKey} = await getTopShaTag({
+           owner : targetOwner,
+           repo:targetRepository,
+           branch:targetBranch
+      }, githubToken);
+
+    const {sha:forkBranchKey, octokit:forkBranchOctokit} = await getTopShaTag({
+          owner : prOwner,
+          repo:prRepository,
+          branch:prBranch
+    }, githubToken);
+
+
+
+    core.setOutput('orgin', mainBranchKey);
+    core.setOutput('fork', forkBranchKey);
+
+    if(mainBranchKey != forkBranchKey){
+      core.debug('Make Pull Request');
+
+         await forkBranchOctokit.request('POST /repos/' + prOwner + '/' + prRepository + '/merge-upstream', {
+            branch:prBranch
+        })
+
+        await forkBranchOctokit.request('POST /repos/' + prOwner + '/' + prRepository + '/pulls', {
+            head:prBranch,
+            base:prBaseBranch,
+            'title' : '<Pull Reuqest> merge Orign[' + mainBranchKey + ']'
+        })
+
+    }
+
   } catch (error) {
+    core.error(error.message);
     core.setFailed(error.message);
   }
+}
+
+async function getTopShaTag(octoConfig, githubToken) {
+
+  const octokit = github.getOctokit(githubToken);
+  const branch = await octokit.rest.repos.getBranch(octoConfig);
+
+  core.debug(branch);
+  core.info(`${octoConfig.owner}${octoConfig.repo} ${octoConfig.branch} - head [${branch.data.commit.sha}]`);
+
+  return {sha:branch.data.commit.sha,
+          octokit : octokit};
 }
 
 run();
